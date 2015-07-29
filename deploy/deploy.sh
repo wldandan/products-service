8657866832ff4003b0b6d5d0ff1a0f5033596e79
Wang Lei <wldandan@gmail.com>
2015-07-29 11:22:40 +0800

    Update Deployment Script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 set -e
 
-[[ -z "$1" ]] && echo "Usage: Please Specify deployment file !!!" && exit 1
+[[ -z "$1" ]] && echo "Usage: Please Specify deployment file." && exit 1
 
 # parse configuration from environment specific file
-echo  "Parsing config file $1 ..."
+echo  "Parsing config file $1..."
 
 # Create resources
-echo "Creating resources ..."
+echo "Creating resources..."
 
 # Pull docker image in the instance created
-echo "Pulling docker image ..."
+echo "Pulling docker image..."
 
 # Run docker container in the instance created
-echo "Runing docker container ..."
+echo "Runing docker container..."
