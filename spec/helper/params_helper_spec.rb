ba8caabfa7ea07e30492ab1fcb13a041379b44ff
Wang lei <wldandan@gmail.com>
2015-10-28 16:39:35 +0800

    Update spec for params_helper

--- spec/helper/params_helper_spec.rb
+++ spec/helper/params_helper_spec.rb
@@ -19,13 +19,11 @@ RSpec.describe ParamsHelper do
 
     subject { params_helper.self_link }
 
-    context 'build self link with params' do
-      before do
-        allow(params_helper).to receive(:request)  { double(base_url: 'http://products-service-host', path_info: '/products', params: { product_id: '1' }) }
-        allow(params_helper).to receive(:declared).and_return(product_id: '1')
-      end
-
-      it { is_expected.to eq 'http://products-service-host/products?product_id=1' }
+    before do
+      allow(params_helper).to receive(:request)  { double(base_url: 'http://products-service-host', path_info: '/products', params: { product_id: '1' }) }
+      allow(params_helper).to receive(:declared).and_return(product_id: '1')
     end
+
+    it { is_expected.to eq 'http://products-service-host/products?product_id=1' }
   end
 end
